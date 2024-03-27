a2c906c6d0f18bc002b0eeb217aa3ed786ca2731
Steven Lu (MBP M1 Max[tmux]) <stevenlu443@gmail.com>
2024-03-28 05:27:34 +0700

    ok progress is good now

--- test/test.ts
+++ test/test.ts
@@ -18,21 +18,22 @@ export const happypath_inception = test(async ({t, l, spawn, f, c, a: {eq, match
   const sourceDir = '/tmp/static_sharing_test_dir_1';
   fs.mkdirSync(sourceDir, {recursive: true});
   const file = path.resolve(sourceDir, 'test.html');
+  const name = 'test';
   // make a source file, simplest possible html file
   fs.writeFileSync(file, `<body><p>hello world</p></body>`);
+  const date = execSync("date +%Y-%m-%d").toString().trim();
+  c(async () => {
+    l('Running cleaning up to erase these test dirs...');
+    await spawn("rm", ["-rf", path.resolve(__dirname, '..', date, name)]);
+    await spawn("rm", ["-rf", sourceDir]);
+  });
 
   // its def gonna be a bit unkosher since running script will tamper with git status.
   
-  const ret = await spawn(path.resolve(__dirname, '..', 'publish.sh'), ['-n', 'test', sourceDir], {bufferStdout: true});
+  const ret = await spawn(path.resolve(__dirname, '..', 'publish.sh'), ['-n', name, sourceDir], {bufferStdout: true, showStdoutWhenBuffering: true});
 
-  const date = execSync("date +%Y-%m-%d").toString();
   const regex = RegExp(`changes added for ${date}/${sourceDir}`);
   match(ret.stdout, regex);
 
-  c(async () => {
-    l('Running cleaning up to erase these test dirs...');
-    await spawn("rm", ["-rf", path.resolve(__dirname, '..', date, sourceDir)]);
-    await spawn("rm", ["-rf", sourceDir]);
-  });
 });
 
