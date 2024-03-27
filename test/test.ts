4fa077760f6fcb8fc875aa589901e4a8c90cfc55
Steven Lu (MBP M1 Max[tmux]) <stevenlu443@gmail.com>
2024-03-28 04:11:13 +0700

    something is not right

--- test/test.ts
+++ test/test.ts
@@ -1,3 +1,4 @@
+import { execSync } from "child_process";
 import fs from "fs";
 import path from "path";
 import { test, LaunchTests } from "tst";
@@ -10,25 +11,28 @@ const isProgramLaunchContext = () => {
 
 isProgramLaunchContext() && LaunchTests(path.resolve(__dirname), {echo_test_logging: true});
 
-export const trivial = test(({t, l}) => {
-  t('exemptFromAsserting');
-  l('hi')
-});
-
 // just doing some controlled inception testing on the main shell script that streamlines this simple github web artifact
 // share hosting project.
 
-export const happypath_inception = test(async ({t, l, spawn}) => {
+export const happypath_inception = test(async ({t, l, spawn, f, c, a: {eq, match}}) => {
   const sourceDir = '/tmp/static_sharing_test_dir_1';
   fs.mkdirSync(sourceDir, {recursive: true});
-  const file = path.resolve(sourceDir, 'test.ts');
+  const file = path.resolve(sourceDir, 'test.html');
   // make a source file, simplest possible html file
   fs.writeFileSync(file, `<body><p>hello world</p></body>`);
 
   // its def gonna be a bit unkosher since running script will tamper with git status.
   
-  await spawn(path.resolve(__dirname, '..', 'publish.sh'), ['-n', 'test', sourceDir]);
+  const ret = await spawn(path.resolve(__dirname, '..', 'publish.sh'), ['-n', 'test', sourceDir], {bufferStdout: true});
 
-  await spawn("rm", ["-rf", sourceDir]);
+  const date = execSync("date +%Y-%m-%d").toString();
+  const regex = RegExp(`changes added for ${date}/${sourceDir}`);
+  match(ret.stdout, regex);
+
+  c(async () => {
+    l('Running cleaning up to erase these test dirs...');
+    await spawn("rm", ["-rf", path.resolve(__dirname, '..', date, sourceDir)]);
+    await spawn("rm", ["-rf", sourceDir]);
+  });
 });
 
